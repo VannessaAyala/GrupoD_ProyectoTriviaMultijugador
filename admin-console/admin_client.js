e7216331513aac273a9458daab312504369fd9a2
unknown <irvingmartinez2004@gmailcom>
2026-05-13 10:32:51 -0500

    feat: agregar conexion TCP al servidor

--- admin-console/admin_client.js
+++ admin-console/admin_client.js
@@ -3,3 +3,9 @@ const readline = require('readline');
 
 const PORT = 4000;
 const HOST = 'localhost';
+
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+    console.log('Admin conectado');
+});
